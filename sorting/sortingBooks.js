653be115fe0334cdc942dfa43f090eaaab5c898c
AL FAJRI PUTRA PRATAMA <72371119+fajriputra@users.noreply.github.com>
2022-06-21 09:04:25 +0700

    Update sortingBooks.js

--- sorting/sortingBooks.js
+++ sorting/sortingBooks.js
@@ -1,8 +1,9 @@
 // // selain mengurutkan buku berdasarkan kategori, ia juga ingin mengurutkan tinggi buku. Selain itu ia hanya membolehkan maksimal 2 eksemplar buku yang sama berada di rak, selebihnya disimpan di gudang penyimpanan. Buku yang diprioritaskan untuk dipajang di rak adalah buku yang paling tinggi ukurannya
 
-// // format karakter pertama -> kategori buku
-// //                 kedua -> judul buku
-// // sisa karakter -> bilangan bulat dari tinggi buku dalam sentimeter
+// format karakter
+// pertama -> kategori buku
+// kedua -> judul buku
+// sisa karakter -> bilangan bulat dari tinggi buku dalam sentimeter
 
 const categoriesBook = [
   { bookId: 6, bookName: "Applied Sciences (600)" },
@@ -33,21 +34,25 @@ function sortingBooks(str) {
     tampung.push(objBooks);
   }
 
-  for (let i = 0; i < tampung.length; i++) {
-    let min = i;
-    console.log(min);
+  console.log(tampung);
 
-    for (let j = i + 1; j < tampung.length; j++) {
-      console.log(j);
-      console.log(categoriesBook[j]);
+  // for (let i = 0; i < tampung.length; i++) {
+  //   let min = i;
+  //   console.log(min);
 
-      // if (tampung[i].kId === categoriesBook[j].bookId) {
-      //   console.log(categoriesBook[i]);
-      //   console.log(tampung[min]);
-      // }
-    }
-  }
+  //   for (let j = i + 1; j < tampung.length; j++) {
+  //     console.log(j);
+  //     console.log(categoriesBook[j]);
+  //     // if (tampung[i].kId === categoriesBook[j].bookId) {
+  //     //   console.log(categoriesBook[i]);
+  //     //   console.log(tampung[min]);
+  //     // }
+  //   }
+  // }
 }
+
+// input 3A13 5X19 9Y20 2C18 1N20 3N20 1M21 1F14 9A21 3N21 0E13 5G14 8A23 9E22 3N14
+// output 0E13 9E22 9A21 9Y20 8A23 1M21 1N20 1F14 2C18 5X19 5G14 3N21 3N20 3A13
 
 console.log(
   sortingBooks(
@@ -55,4 +60,3 @@ console.log(
   ),
   "0E13 9E22 9A21 9Y20 8A23 1M21 1N20 1F14 2C18 5X19 5G14 3N21 3N20 3A13"
 );
-// module.exports = sortingBooks;
